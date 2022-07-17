d93fbc2d72653e13be53f26f5a490e16a6112173
eduar <eduardvitko>
2022-07-17 13:40:41 +0300

    Setting project: adding maven dependencies and database properties

---
+++ InternetProvider/src/main/java/com/ProjectInternet/InternetProvider/InternetProviderApplication.java
@@ -0,0 +1,13 @@
+package com.ProjectInternet.InternetProvider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetProviderApplication.class, args);
+	}
+
+}
---
+++ InternetProvider/src/main/java/com/models/User.java
@@ -0,0 +1,40 @@
+package com.models;
+
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotEmpty(message = "mandatoryParametersMissing")
+    @NotNull
+    @NotBlank
+    @Pattern(regexp = "^[0-9]{12}$")
+    //@SqlInjectionSafe
+    @Column(unique = true, nullable = false, updatable = false)
+    private String phone;
+
+    @Column
+    private boolean isActive;
+    @Column
+    private Role role;
+    @Column
+    private LocalDateTime created;
+    @Column
+    private LocalDateTime updated;
+}
